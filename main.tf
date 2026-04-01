a2d3111db67cf1aa452b3f3acfc1219618dc8dab
Marksmanship <AustinK.Copeland@gmail.com>
2026-04-01 11:49:45 -0400

    conntrack fix attempt 2

--- main.tf
+++ main.tf
@@ -154,8 +154,9 @@ locals {
     apt-mark hold kubelet kubeadm kubectl
     systemctl enable kubelet
 
-    apt-get update
-    apt-get install -y conntrack
+    kubeadm reset -f
+    apt update
+    apt install -y conntrack
 
     # Initialize the cluster
     kubeadm init \
@@ -220,8 +221,9 @@ locals {
     apt-mark hold kubelet kubeadm kubectl
     systemctl enable kubelet
 
-    apt-get update
-    apt-get install -y conntrack
+    kubeadm reset -f
+    apt update
+    apt install -y conntrack
 
     # The worker will need to join the cluster manually after the control plane is ready.
     # SSH into the control plane, get the join command from /home/ubuntu/join-command.sh,
